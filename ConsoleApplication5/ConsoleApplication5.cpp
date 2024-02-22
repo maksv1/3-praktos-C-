174ca94131a9295baa208ccc9863a50093e54d6e
maksv1 <isip_m.v.vinogorov@mpt.ru>
2024-02-22 23:07:17 +0300

    Практос 3 по C++

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    double matrix[3][5];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << "Введите элемент массива [" << i << "][" << j << "]: ";
+            cin >> matrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        double sum = 0;
+        for (int j = 0; j < 5; j++) {
+            sum += matrix[i][j];
+        }
+        double average = sum / 5;
+        cout << "Среднее арифметическое элементов строки " << i << ": " << average << endl;
+    }
+
+    return 0;
+}
